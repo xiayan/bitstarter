4b8c28742ab3b2b422c5331a1a49d070b7060d32
Yan Xia <drseanxy@mac.com>
2013-07-17 16:42:24 -0500

    Modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs')
 
 var app = express.createServer(express.logger());
 
+var buf = fs.readFileSync("index.html")
+var content = buf.toString('utf8', 0, buf.length)
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
